1f44402bd1e211f7414f4efd861160ef0e705217
railsfactory-pramod <pramod@railsfactory.com>
2012-03-16 12:44:25 +0530

    Minor changes

--- app/controllers/bolt/info_controller.rb
+++ app/controllers/bolt/info_controller.rb
@@ -2,16 +2,17 @@ module Bolt
   class InfoController < Bolt::BoltController
     layout "default_site"
     def show      
+      @top_menu = Page.parent_menu
       if(!params[:cat_name].nil?)
         if(!params[:cat_name].match(/article/i).nil?)
           if(!params[:id].nil?)
             @article = Article.find(params[:id].to_i)
           end
         else
+          @articles = Article.published
         end      
       else
-        @categories = Category.all
-        @top_menu = Page.parent_menu      
+        @categories = Category.all        
         if(params[:id].nil? || !params[:id].match(/index|home/i).nil?)
           @page = Page.find_by_title("Home")        
         else      
